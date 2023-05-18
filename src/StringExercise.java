5c29eaa194165079d762eabf023a1d3255daa169
Gianna Garces <117394492+Gianna-Garces@users.noreply.github.com>
2023-05-18 17:16:36 -0500

    completed part 1 and 2 of EX 1

---
+++ src/StringExercise.java
@@ -0,0 +1,16 @@
+public class StringExercise {
+    public static void main(String [] args) {
+        String output1 = "We don't need no education";
+        System.out.println(output1);
+
+        String output2 = "We don't need no though control";
+        System.out.println(output2);
+
+        String output3 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(output3);
+
+//        String output4 = "In windows, the main drive is usually C:\";
+
+
+    }
+}
