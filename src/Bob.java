58e4b40cb510517b1ad5543c3585cca014438397
Gianna Garces <117394492+Gianna-Garces@users.noreply.github.com>
2023-05-19 00:06:00 -0500

    String Exercises Completed

---
+++ src/Bob.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        Scanner userQuery = new Scanner(System.in);
+
+        boolean user = true;
+
+        while(user) {
+
+            System.out.println("\n Let's talk to Bob.  Say something to Bob.");
+
+            String query = userQuery.nextLine();
+            if (query.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if (query.endsWith("?")){
+                System.out.println("Sure.");
+            } else if (query == ""){
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever");
+            }
+        }
+    }
+}
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,16 +1,15 @@
 public class StringExercise {
     public static void main(String [] args) {
-        String output1 = "We don't need no education";
+        String output1 = "We don't need no education\nWe don't need no though control";
         System.out.println(output1);
 
-        String output2 = "We don't need no though control";
+        String output2 = "Check \"this\" out!, \"s inside of \"s!";
         System.out.println(output2);
 
-        String output3 = "Check \"this\" out!, \"s inside of \"s!";
+        String output3 = "In windows, the main drive is usually C:\\";
         System.out.println(output3);
 
-//        String output4 = "In windows, the main drive is usually C:\";
-
-
+        String output4 = "I can do backslashes \\, double backslashes \\\\, \nand the amazing triple backslash \\\\\\!";
+        System.out.println(output4);
     }
 }
